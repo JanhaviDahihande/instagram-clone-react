feb2214f098f619f32a278ad8b6ac2f70105d83d
janhavi <janhavi.dahihande@oculid.com>
2020-07-19 21:27:42 -0700

    Add Instagram embed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Button, Input } from '@material-ui/core';
 import ImageUpload from './ImageUpload';
+import InstagramEmbed from 'react-instagram-embed';
 
 function getModalStyle() {
   const top = 50;
@@ -162,21 +163,39 @@ function App() {
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt=""
         />
+        { user ? (
+          <Button onClick={() => auth.signOut() }>Logout</Button>
+        ): (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
       </div>
-      { user ? (
-        <Button onClick={() => auth.signOut() }>Logout</Button>
-      ): (
-        <div className="app__loginContainer">
-          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
-          <Button onClick={() => setOpen(true)}>Sign Up</Button>
-        </div>
-      )}
       {/* Posts */}
-      {
-        posts.map((post) => (
-          <Post username={post.username} imageUrl={post.imageUrl} caption={post.caption}/>
-        ))
-      }
+      <div className="app__posts">
+        <div className="app__postsLeft">
+          {
+            posts.map((post) => (
+              <Post username={post.username} imageUrl={post.imageUrl} caption={post.caption}/>
+            ))
+          }
+        </div>
+        <div className="app__postsRight">
+          <InstagramEmbed
+            url='https://www.instagram.com/p/CCXRgM4DPP7/'
+            maxWidth={320}
+            hideCaption={false}
+            containerTagName='div'
+            protocol=''
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
+          />
+        </div>
+      </div>
       {/* Do not display unless the user has signed in */}
       { user?.displayName && <ImageUpload username={user.displayName}/>}
     </div>
